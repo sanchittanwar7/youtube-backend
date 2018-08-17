7c188ef62adfd95db866646c2351e27f783d67e7
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-08-17 23:54:33 +0530

    download corrected

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+path = require('path');
 var youtubedl = require('youtube-dl');
- 
+
 /* GET home page. */
-router.get('/download/:id/:format', function(req, res, next) {
+router.get('/download/:id/:format/:filename', function (req, res, next) {
 	var id = req.params.id;
 	var format = req.params.format;
-	console.log(id)
-	console.log(format)
+	var filename = req.params.filename;
 	var video = youtubedl('https://www.youtube.com/watch?v=' + id, ['--format=' + format]);
 	var title;
-	video.on('info', function(info) {
+	video.on('info', function (info) {
 		console.log('Download started');
-		console.log('filename: ' + info.filename);
-		console.log('size: ' + info.size/1000000 + 'MB');
-		// var filename = encodeURIComponent(info.title + '.mp4');
-		video.pipe(fs.createWriteStream(info._filename));
+		console.log('filename: ' + info._filename);
+		console.log('size: ' + info.size / 1000000 + 'MB');
+		video.pipe(fs.createWriteStream(filename));
+
 	});
 
-	// video.pipe(fs.createWriteStream(title));
-	video.on('end', function() {
-	  console.log('finished downloading!');
-	  res.end()
+	video.on('end', function () {
+		var pathname = path.join(__dirname, '../')
+		console.log('finished downloading!');
+		res.end()
 	});
+
 });
+
+router.get('/getit/:filename', function(req, res, next) {
+	var filename = req.params.filename;
+	var pathname = path.join(__dirname, `../${filename}`);
+	console.log('transfering ', filename, ' ', pathname)
+	res.download(pathname, filename); 
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+path = require('path');
 var youtubedl = require('youtube-dl');
 
-/* GET users listing. */
-router.get('/download/:id/:format', function(req, res, next) {
+/* GET home page. */
+router.get('/download/:id/:format', function (req, res, next) {
 	var id = req.params.id;
 	var format = req.params.format;
-	console.log(id)
-	console.log(format)
+	var filename;
 	var video = youtubedl('https://www.youtube.com/watch?v=' + id, ['--format=' + format]);
 	var title;
-	video.on('info', function(info) {
+	video.on('info', function (info) {
 		console.log('Download started');
-		console.log('filename: ' + info.filename);
-		console.log('size: ' + info.size);
-		// var filename = encodeURIComponent(info.title + '.mp4');
+		console.log('filename: ' + info._filename);
+		console.log('size: ' + info.size / 1000000 + 'MB');
+		filename = info._filename;
 		video.pipe(fs.createWriteStream(info._filename));
+		var tryFetch = {myString: filename};
+		res.send(tryFetch);
+
 	});
 
-	// video.pipe(fs.createWriteStream(title));
-	video.on('end', function() {
-	  console.log('finished downloading!');
-	  res.end()
+	video.on('end', function () {
+		var pathname = path.join(__dirname, '../')
+		console.log('finished downloading!');
+
 	});
 
 });
 
+router.get('/getit', function(req, res, next) {
+
+})
+
 module.exports = router;
-
-
-
-
-
